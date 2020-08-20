cf7d26df0ea4494cd7cb028bef04d00378e45309
GregDupl <gdu.contact@gmail.com>
2020-08-20 15:54:27 +0200

    tests code 200 for basic views (index, webmarket...)

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from store.models import *
+from store.management.commands.create_data import *
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.test import TestCase, Client
 
 # Create your tests here.
+class IndexTestCase(TestCase):
+    def test_index(self):
+        c = Client()
+        response = c.get(reverse("store:index"))
+        self.assertEqual(response.status_code, 200)
+
+class WebmarketTestCase(TestCase):
+    def test_index(self):
+        c = Client()
+        response = c.get(reverse("store:webmarket"))
+        self.assertEqual(response.status_code, 200)
+
+class MarketplaceTestCase(TestCase):
+    def test_index(self):
+        c = Client()
+        response = c.get(reverse("store:marketplaces"))
+        self.assertEqual(response.status_code, 200)
+
+class CartTestCase(TestCase):
+    def test_index(self):
+        c = Client()
+        response = c.get(reverse("store:cart"))
+        self.assertEqual(response.status_code, 200)
