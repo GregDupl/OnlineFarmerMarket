253a0fd066bd5e2978559c91ffcc201a03a75a7b
GregDupl <gdu.contact@gmail.com>
2020-08-20 17:14:42 +0200

    resolve import conflict with "Client" from models and "Client()" from django

--- store/tests.py
+++ store/tests.py
@@ -1,31 +1,31 @@
 from store.models import *
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.test import TestCase, Client
+from django.test import TestCase, Client as C
 from store.management.commands.create_data import *
 
 # Create your tests here.
 class IndexTestCase(TestCase):
     def test_index(self):
-        c = Client()
+        c = C()
         response = c.get(reverse("store:index"))
         self.assertEqual(response.status_code, 200)
 
 class WebmarketTestCase(TestCase):
     def test_index(self):
-        c = Client()
+        c = C()
         response = c.get(reverse("store:webmarket"))
         self.assertEqual(response.status_code, 200)
 
 class MarketplaceTestCase(TestCase):
     def test_index(self):
-        c = Client()
+        c = C()
         response = c.get(reverse("store:marketplaces"))
         self.assertEqual(response.status_code, 200)
 
 class CartTestCase(TestCase):
     def test_index(self):
-        c = Client()
+        c = C()
         response = c.get(reverse("store:cart"))
         self.assertEqual(response.status_code, 200)
 
