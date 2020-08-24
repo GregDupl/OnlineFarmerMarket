f5207a16790fb940392f85e6bfd84c8adeed3cdd
GregDupl <gdu.contact@gmail.com>
2020-08-24 15:15:27 +0200

    adaptative form to connect or create account depending type of client

--- store/static/store/js/script.js
+++ store/static/store/js/script.js
@@ -13,8 +13,6 @@ $(".filtres").on('click', function (event){
 })
 
 
-
-
 $('#detailProduct').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var name = button.data('product') + " - " + button.data('variety') // Extract info from data-* attributes
@@ -35,9 +33,94 @@ $('#detailProduct').on('show.bs.modal', function (event) {
 })
 
 
-
 $("#carouselExampleCaptions").on('slid.bs.carousel', function () {
    var vids = $(this).find(".active video");
    vids[0].currentTime = 0;
    vids[0].play()
 })
+
+
+// events for adaptative and dynamic login form in modal
+
+$('#ModalLogin').on('show.bs.modal', function (event) {
+  $('#have_account').prop('checked', true);
+  $("#create_account").hide();
+  $(":input.req").prop('required',false);
+  $("#confirmpass").hide();
+})
+
+// adaptative form if connect or create account
+$("input[name='formchoice']").click(function(){
+  if ($('#have_account').is(':checked')) {
+    $("#create_account").hide();
+    $(":input.req").prop('required',false);
+    $("#confirmpass").hide();
+  } else if ($('#no_account').is(':checked')) {
+    $("#create_account").show();
+    $(":input.req").prop('required',true);
+    $('#partic').prop('checked', true);
+    $("#type_account label[for='name']").html("Nom *");
+    $("#type_account input[name='phone']").prop('required',false);
+    $("#type_account label[for='phone']").html("Téléphone");
+    $("#confirmpass").show();
+  }
+})
+
+// adaptative create form depending client type
+$("input[name='clienttype']").click(function(){
+  if ($('#partic').is(':checked')) {
+    $("#type_account label[for='name']").html("Nom *");
+    $("#type_account input[name='phone']").prop('required',false);
+    $("#type_account label[for='phone']").html("Téléphone");
+  } else if ($('#rest').is(':checked')) {
+    $("#restaurantname").show();
+    $("#type_account input[name='phone']").prop('required',true);
+    $("#type_account label[for='phone']").html("Téléphone *");
+    $("#type_account label[for='name']").html("Restaurant *");
+  }
+})
+
+
+const login = () => {
+  $.ajax({
+    type:"POST",
+    headers:{'X-CSRFToken': $('[name=csrfmiddlewaretoken]').val()},
+    url:$("#loginform").attr('action'),
+    data : data,
+    success: $("#ModalLogin").modal('hide'),
+    dataType : "json"
+  });
+};
+
+
+$("#loginform").submit(function(event){
+  event.preventDefault();
+  if (($('#have_account').is(':checked'))) {
+    data = {
+      "choice" : $('input[name="formchoice"]:checked').val(),
+      "email" : $("input[name='mail']").val(),
+      "password" : $("input[name='password']").val()
+    }
+    login()
+  }
+  else if (($('#no_account').is(':checked'))) {
+    if ($("input[name='password']").val() == $("input[name='confirmpassword']").val()) {
+      data = {
+        "choice" : $('input[name="formchoice"]:checked').val(),
+        "type" : $('input[name="clienttype"]:checked').val(),
+        "email" : $("input[name='mail']").val(),
+        "name" : $("input[name='name']").val(),
+        "phone": $("input[name='phone']").val(),
+        "number": $("input[name='number']").val(),
+        "street" : $("input[name='rue']").val(),
+        "cp" : $("input[name='code_postal']").val(),
+        "city" : $("input[name='ville']").val(),
+        "password" : $("input[name='password']").val()
+      }
+      login()
+    } else {
+      $('#alertmdp').html('Veuillez saisir un même mot de passe')
+    }
+  }
+
+});
