5e8b3e81543eb5b9a2f8dd5ffeb04fabeb0d4b64
GregDupl <gdu.contact@gmail.com>
2020-08-24 18:42:34 +0200

    view for login or create client v1

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from store.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
 # Create your views here.
 def index(request):
     return render(request, 'store/index.html')
@@ -33,7 +36,44 @@ def cart(request):
     return render(request,'store/cart.html')
 
 def login(request):
-    data = request.POST
-    print(data)
+    email = request.POST['email']
+    password = request.POST['password']
 
-    return render(request, 'store/index.html')
+    if request.POST['choice'] == 'connect':
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            message = "succes to connect"
+        else:
+            message = "incorrect id"
+
+    elif request.POST['choice'] == 'create':
+        name = request.POST['name']
+        u = User.objects.filter(email=email)
+        if u.exists():
+            message = "already exists"
+        else:
+            User.objects.create_user(name, email, password)
+
+            number = request.POST['number']
+            street = request.POST['street']
+            cp = request.POST['cp']
+            city =  request.POST['city']
+
+            adress_from = Adress.objects.filter(numero = number, rue=street, code_postal=cp, ville=city)
+            if not adress_form.exists():
+                adress_from = Adress.objects.create(numero = number, rue=street, code_postal=cp, ville=city)
+
+            Client.objects.create(
+            user = User.objects.get(email=email),
+            phone = request.POST['phone'],
+            fk_client_type = ClientType.objects.get(type_client=request.POST['type']),
+            fk_adress = Adress.objects.get(numero = number, rue=street, code_postal=cp, ville=city)
+            )
+
+            message = "new client created"
+
+    print(message)
+    context = {"message": message}
+
+    return JsonResponse(context)
