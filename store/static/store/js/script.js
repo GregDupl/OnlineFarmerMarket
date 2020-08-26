b90ff0fdd370dfbc43de61c84c8db21a1f24cbe5
GregDupl <gdu.contact@gmail.com>
2020-08-26 15:33:17 +0200

    ajax post request in jquery to send data (idproduct+quantity)

--- store/static/store/js/script.js
+++ store/static/store/js/script.js
@@ -17,19 +17,21 @@ $('#detailProduct').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var name = button.data('product') + " - " + button.data('variety') // Extract info from data-* attributes
   var prix = button.data('price') + "€ / " + button.data('unity')
+  var id_variety = button.data('id')
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
   modal.find('.modal-title').text(name)
   modal.find('.price').text(prix)
+  modal.find('#quantity').attr("data-id", id_variety)
 
-  var vids = $(".videorecette");
-  for (var i = 0; i < vids.length; i++) {
-    vids[i].load()
-  }
+  //var vids = $(".videorecette");
+  //for (var i = 0; i < vids.length; i++) {
+//    vids[i].load()
+  //}
 
-  var active = $(this).find(".active video");
-  active[0].play()
+//  var active = $(this).find(".active video");
+//  active[0].play()
 })
 
 
@@ -125,3 +127,19 @@ $("#loginform").submit(function(event){
   }
 
 });
+
+$('#cartform').submit(function(event){
+  event.preventDefault();
+  $.ajax({
+    type:"POST",
+    headers:{'X-CSRFToken': $('[name=csrfmiddlewaretoken]').val()},
+    url:$("#cartform").attr('action'),
+    data :
+    {
+      "quantity" : $('#quantity').val(),
+      "product" : $('#quantity').attr('data-id'),
+    },
+    dataType : "json"
+  });
+
+})
