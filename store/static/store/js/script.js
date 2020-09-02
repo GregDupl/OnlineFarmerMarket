5262a9c02b1de3b5e659cf56544524530526f01c
GregDupl <gdu.contact@gmail.com>
2020-09-02 17:27:45 +0200

    js : creation of a cart interface for add or modify value in user cart

--- store/static/store/js/script.js
+++ store/static/store/js/script.js
@@ -11,7 +11,7 @@ $(".filtres").on('click', function (event){
       $(product[i]).hide()
     }
   }
-})
+});
 
 
 // MODAL
@@ -25,14 +25,24 @@ $('#detailProduct').on('show.bs.modal', function (event) {
   modal.find('.price').text(
     product.data('price') + "€ / " + product.data('unity')
   )
-  modal.find('#quantity').attr("data-id", product.data('id'))
-  modal.find('#quantity').attr("max", product.data('stock'))
+  input = modal.find('input')
+  input.parents(':eq(1)').attr("data-id", product.data('id'))
+  input.attr("max", product.data('stock'))
 
-  if (product.data('cart')) {
-    modal.find('#quantity').val(product.attr('data-cart'))
+  if (product.attr('data-cart')) {
+    input.attr("value",product.attr('data-cart'))
+    input.val(product.attr('data-cart'))
+    input.parent("div").css("display","block")
+    modal.find('.add').css("display","none")
+    minus = modal.find(".minus")
+    plus = modal.find(".plus")
+    update_button(input.attr("value"), input.attr("max"), minus, plus)
   }
   else{
-    modal.find('#quantity').val(1)
+    input.attr("value",1)
+    input.val(1)
+    input.parent("div").css("display","none")
+    modal.find('.add').css("display","block")
   }
 
   //var vids = $(".videorecette");
@@ -50,29 +60,6 @@ $("#carouselExampleCaptions").on('slid.bs.carousel', function () {
    vids[0].play()
 })
 
-$('#cartform').submit(function(event){
-  event.preventDefault();
-  quantity = $('#quantity').val()
-  product = $('#quantity').attr('data-id')
-  $.ajax({
-    type:"POST",
-    headers:{'X-CSRFToken': $('[name=csrfmiddlewaretoken]').val()},
-    url:$("#cartform").attr('action'),
-    data :
-    {
-      "quantity" : quantity,
-      "product" : product,
-    },
-    success:function(){
-      $("div[data-id="+product+"]").attr('data-cart', quantity);
-      button = $('#detailProduct').find("#add");
-      var initial_text = button.html();
-      button.html("Ajouté !");
-      setTimeout(function(){button.html(initial_text);}, 1200);
-    },
-    dataType : "json"
-  });
-});
 
 //LOGIN FORM
 
@@ -162,6 +149,8 @@ $("#loginform").submit(function(event){
 });
 
 
+
+// ****** CART INTERFACE ******
 function update_total_cart() {
   var total_result=0;
   var subtotal = $(".subtotal");
@@ -172,55 +161,206 @@ function update_total_cart() {
   $("#total_cart").text(total_result.toFixed(2))
 };
 
-//remove from Cart
-$('.remove').on('click', function(event) {
-  event.preventDefault()
-  var button = $(event.target);
+function update_cart(origin, value){
+  subtotal = origin.parents("tr").find(".subtotal");
+  price = origin.parents("tr").attr("data-price");
+  result = price * value;
+  subtotal.text(result.toFixed(2));
+  update_total_cart()
+};
 
+function update_button(value, max, minus, plus){
+  if (value == 1)
+  {
+    minus.text("del")
+    minus.addClass("del")
+  }
+  else
+  {
+    minus.text("-")
+    minus.removeClass("del")
+  }
+
+  if (value == max)
+  {
+    plus.text("max")
+    plus.prop("disabled", true)
+  }
+  else
+  {
+    plus.text("+")
+    plus.prop("disabled", false)
+  }
+};
+
+function ajax_update(input, id_product, value, action){
   $.ajax({
     type:"POST",
     headers:{'X-CSRFToken': $('[name=csrfmiddlewaretoken]').val()},
-    url:$(button).parents("table").attr('data-url'),
+    url:input.parent("div").attr('data-url'),
     data : {
-      "cart_object" : button.parents("tr").attr('data-product'),
-      "action" : "remove"
-    },
-    success : function() {
-      $(button).parents("tr").remove();
-      update_total_cart()
-    },
-
-    dataType : "json"
-  });
-
-  return false;
-})
-
-// Update quantity in cart
-$('table input[type="number"]').change(function(event) {
-  origin = $(event.target);
-  $.ajax({
-    type:"POST",
-    headers:{'X-CSRFToken': $('[name=csrfmiddlewaretoken]').val()},
-    url:$(origin).parents("table").attr('data-url'),
-    data : {
-      "cart_object" : origin.parents("tr").attr('data-product'),
-      "quantity" : origin.val(),
-      "action" : "update"
+      "cart_object" : id_product,
+      "quantity" : value,
+      "action" : action
     },
     error : function() {
       alert("error")
     },
     success:function(){
-      subtotal = origin.parents("tr").find(".subtotal");
-      price = origin.parents("tr").attr("data-price");
-      result = price * origin.val();
-      subtotal.text(result.toFixed(2));
-      update_total_cart()
+      input.attr("value",value)
+      input.val(value)
+      minus = input.siblings(".minus")
+      plus = input.siblings(".plus")
+      max = input.attr("max")
+      update_button(value, max, minus, plus)
+
+      if ($.contains(document.body, $(".table")[0]))
+      {
+        update_cart(input, value)
+      }
+      else
+      {
+        product_catalog = $(".img_product[data-id="+product+"]")
+        product_catalog.attr('data-cart', value)
+
+        if ($("body").hasClass("modal-open"))
+        {
+          product_catalog_cart = product_catalog.parent().find(".cart_interface")
+          input_catalog = product_catalog_cart.find("input")
+          input_catalog.attr('value', value)
+          input_catalog.val(value)
+          minus = input_catalog.siblings(".minus")
+          plus = input_catalog.siblings(".plus")
+          update_button(value, max, minus, plus)
+        }
+      }
+    },
+    dataType : "json"
+  });
+};
+
+function ajax_remove(input, id_product, action){
+  $.ajax({
+    type:"POST",
+    headers:{'X-CSRFToken': $('[name=csrfmiddlewaretoken]').val()},
+    url:input.parent(".modif").attr('data-url'),
+    data : {
+      "cart_object" : id_product,
+      "action" : action
+    },
+    error : function() {
+      alert("error")
+    },
+    success:function(){
+      if ($.contains(document.body, $(".table")[0]))
+      {
+        input.parents("tr").remove();
+        update_total_cart()
+      }
+      else
+      {
+        var product_catalog = $(".img_product[data-id="+id_product+"]")
+        product_catalog.removeAttr('data-cart')
+        var cart_elt = input.parents('.cart_interface')
+        cart_elt.find(".modif").css("display","none")
+        cart_elt.find(".add").css("display","block")
+
+        if ($("body").hasClass("modal-open"))
+        {
+          var cart_catalog = product_catalog.parent().find('.cart_interface')
+          cart_catalog.find(".modif").css("display","none")
+          cart_catalog.find(".add").css("display","block")
+        }
+      }
+    },
+    dataType : "json"
+  });
+}
+
+$(".add").click(function(event){
+  add_button = $(event.target)
+  cart_elt = add_button.parent('.cart_interface')
+  quantity = 1
+  product = cart_elt.attr('data-id')
+  $.ajax({
+    type:"POST",
+    headers:{'X-CSRFToken': $('[name=csrfmiddlewaretoken]').val()},
+    url:add_button.attr('data-url'),
+    data :
+    {
+      "quantity" : quantity,
+      "product" : product,
+    },
+    success:function(){
+      modify_cart = $(cart_elt.find(".modif"))
+      modify_cart.css('display', 'block')
+      add_button.css('display','none')
+
+      product_catalog = $(".img_product[data-id="+product+"]")
+      product_catalog.attr('data-cart', quantity);
+
+      if ($("body").hasClass("modal-open")) {
+        var cart_catalog = product_catalog.parent().find(".cart_interface")
+        cart_catalog.find(".modif").css("display","block")
+        cart_catalog.find(".add").css("display","none")
+        cart_catalog.find(".minus").text("del")
+        cart_catalog.find(".minus").addClass("del")
+      }
 
     },
     dataType : "json"
   });
+});
 
-  return false;
-})
+$(".changebutton").click(function(event){
+  modify_button = $(event.target)
+  cart_elt = modify_button.parents(':eq(1)')
+  product = cart_elt.attr('data-id')
+  input = cart_elt.find("input")
+  initial_value = parseInt( input.attr('value') )
+  max = parseInt( input.attr("max") )
+
+  if (modify_button.hasClass("plus")) {
+    expectedvalue = initial_value+1
+  }else{
+    expectedvalue = initial_value-1
+  }
+  if(expectedvalue > max){
+    expectedvalue = max
+  } else if (expectedvalue <= 0) {
+    expectedvalue = 1
+  }
+
+  if (modify_button.hasClass("del")) {
+    ajax_remove(input, product, "remove")
+  }else{
+    ajax_update(input, product, expectedvalue, "update")
+  }
+});
+
+$(".quantity").change(function(event){
+  input = $(event.target);
+  user_entry = parseInt(input.val());
+  max = parseInt(input.attr("max"));
+
+  if ((user_entry <= 0) || (isNaN(user_entry))){
+    input.val(1)
+    input.attr('value', 1)
+  }
+  else if (user_entry > max) {
+    input.val(max)
+    input.attr('value', max)
+  }
+  else{
+    input.attr('value',user_entry)
+  }
+
+});
+
+$(".quantity").focusout(function(e){
+  input = $(event.target)
+  cart_elt = input.parents(':eq(1)')
+  product = cart_elt.attr('data-id')
+  value = parseInt(input.attr('value'))
+  ajax_update(input, product, value, "update")
+});
