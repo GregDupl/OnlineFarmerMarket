55447d850869e56441ae6c8a523d76651dbb0465
GregDupl <gdu.contact@gmail.com>
2020-09-22 11:50:12 +0200

    custom django admin (title and language)

--- marche_en_ligne/urls.py
+++ marche_en_ligne/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+admin.site.site_header = 'Aux jardins gourmands - Administration'
+admin.site.index_title = 'Marché en ligne fermier'
 
 urlpatterns = [
     path('', include('store.urls')),
